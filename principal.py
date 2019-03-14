39e2939e91b299fbb1a6c45be663c6ec2fc13205
dayanOrlando <dayan@gatilabs.com.br>
2019-03-14 17:46:51 -0300

    add test de subtração

--- principal.py
+++ principal.py
@@ -4,3 +4,6 @@
 def somar(x,y):
     return (x+y)
 
+def subtrair(x,y):
+    return (x-y)
+
--- test.py
+++ test.py
@@ -4,3 +4,6 @@ from principal import somar
 def test_somar():
     assert somar(2,4) == 6
 
+def test_subtrair():
+    assert subtrair(4,2) == 2
+
