b30cb8d4efb26d1567521970ed15f579b7bfafec
dayanOrlando <dayan@gatilabs.com.br>
2019-03-15 12:35:25 -0300

    add metodo de mult

--- principal.py
+++ principal.py
@@ -7,3 +7,6 @@ def somar(x,y):
 def sub(x,y):
     return (x-y)
 
+def mult(x,y):
+    return(x,y)
+
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import pytest
-from principal import somar
-from principal import sub
+from principal import * 
 
 def test_somar():
     assert somar(2,4) == 6
 
 def test_subtrair():
-    assert sub(4,2) == 2
+    assert sub(4,2)   == 2
 
+def test_mult():
+    assert mult(2,2)  == 4
